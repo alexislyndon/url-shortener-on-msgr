3da02acc83fbec23e34281499d5a844c6195a31b
Alexis Lyndon Galaura <alexislyndon@gmail.com>
2020-08-17 16:16:22 +0800

    hambara

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
-  res.end("<h1>Hello World</h1>");
+  res.end("<h1>Hambarashingga</h1>");
 });
 
 // server.listen(port, () => {
