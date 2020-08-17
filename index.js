60b155a1236ede1f72ca64a1b8795c9a4ecbe2ae
Alexis Lyndon Galaura <alexislyndon@gmail.com>
2020-08-17 17:56:53 +0800

    page access token env

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 "use strict";
+require('dotenv').config()
+const PAGE_ACCESS_TOKEN = process.env.FACEBOOK_PAGE_ACCESS_TOKEN;
 // const urlchecker = require('is-url');
 
 // const http = require("http");
@@ -90,7 +92,6 @@ app.get("/webhook", (req, res) => {
 function handleMessage(sender_psid, received_message) {
   //   let response;
 
-
   // Check if the message contains text
   if (received_message.text) {
     var shortUrl = require("node-url-shortener");
@@ -110,4 +111,29 @@ function handleMessage(sender_psid, received_message) {
 function handlePostback(sender_psid, received_postback) {}
 
 // Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {}
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    recipient: {
+      id: sender_psid,
+    },
+    message: response,
+  };
+
+  // Send the HTTP request to the Messenger Platform
+  request(
+    {
+      uri: "https://graph.facebook.com/v2.6/me/messages",
+      qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: request_body,
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log("message sent!");
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }
+  );
+}
